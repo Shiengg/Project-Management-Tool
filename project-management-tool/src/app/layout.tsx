a81bff4eb1d063918baec0f0f282b1477e0af7be
Xuana252 <notfuny4927@gmail.com>
2025-06-15 17:36:13 +0700

    add theme background

--- project-management-tool/src/app/layout.tsx
+++ project-management-tool/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import "../styles/themes.css";
 import ThemeManager from "@/components/theme/ThemeManager";
 import Provider from "@/provider/Provider";
 import { Toaster } from "sonner";
